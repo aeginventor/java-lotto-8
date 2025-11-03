82d0c5a4fa9b7bd56ce917e0cc7f2774fc0e1176
San <aeginventor@gmail.com>
2025-11-03 19:50:09 +0900

    test: LottoService (통계 계산) TDD - calculateResults 기능 테스트 추가

--- src/test/java/lotto/LottoServiceTest.java
+++ src/test/java/lotto/LottoServiceTest.java
@@ -3,6 +3,7 @@ package lotto;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -19,4 +20,32 @@ class LottoServiceTest {
 
         assertThat(lottos).hasSize(8);
     }
+
+    @DisplayName("당첨 번호와 로또 목록을 비교하여 정확한 통계(LottoResult)를 반환한다.")
+    @Test
+    void calculateResults() {
+        LottoService lottoService = new LottoService();
+
+        Lotto winningLotto = new Lotto(List.of(1, 2, 3, 4, 5, 6));
+        int bonusNumber = 7;
+
+        List<Lotto> userLottos = Arrays.asList(
+                new Lotto(List.of(1, 2, 3, 4, 5, 6)), // 1등
+                new Lotto(List.of(1, 2, 3, 4, 5, 7)), // 2등
+                new Lotto(List.of(1, 2, 3, 4, 5, 8)), // 3등
+                new Lotto(List.of(1, 2, 3, 4, 8, 9)), // 4등
+                new Lotto(List.of(1, 2, 3, 8, 9, 10)),// 5등
+                new Lotto(List.of(10, 11, 12, 13, 14, 15)) // 꽝(MISS)
+        );
+
+        LottoResult result = lottoService.calculateResults(userLottos, winningLotto, bonusNumber);
+
+        assertThat(result.getCount(Rank.FIRST)).isEqualTo(1);
+        assertThat(result.getCount(Rank.SECOND)).isEqualTo(1);
+        assertThat(result.getCount(Rank.THIRD)).isEqualTo(1);
+        assertThat(result.getCount(Rank.FOURTH)).isEqualTo(1);
+        assertThat(result.getCount(Rank.FIFTH)).isEqualTo(1);
+        assertThat(result.getCount(Rank.MISS)).isEqualTo(1);
+    }
+}
 }
