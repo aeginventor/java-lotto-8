65220dc287dc520198285af0f76d3465787447f7
San <aeginventor@gmail.com>
2025-11-03 19:21:52 +0900

    feat: Lotto.contains() 메서드 구현

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -49,6 +49,10 @@ public class Lotto {
         }
     }
 
+    public boolean contains(int number) {
+        return numbers.contains(number);
+    }
+
     @Override
     public String toString() {
         return numbers.toString();
