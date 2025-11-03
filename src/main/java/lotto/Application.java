3385f6ac55c47ddc708c079c50112fea20f20f4a
San <aeginventor@gmail.com>
2025-11-03 18:42:53 +0900

    feat: 구입 금액 입력 및 예외 발생 시 재입력 기능 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,28 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            int purchaseAmount = getPurchaseAmountWithValidation();
+            // TODO
+        } catch (IllegalArgumentException e) {
+
+        }
+    }
+
+    private static int getPurchaseAmountWithValidation() {
+        while (true) {
+            System.out.println("구입금액을 입력해 주세요.");
+            String input = Console.readLine();
+
+            try {
+                Validator.validatePurchaseAmount(input);
+                return Integer.parseInt(input);
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
